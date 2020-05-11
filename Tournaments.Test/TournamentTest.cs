4261b299ffea66a6b81bdd7b063b2b2578d8839f
Данил Посохов <lyudmila.gartman99@mail.ru>
2020-05-11 04:35:32 +0400

    Добавьте файлы проекта.

---
+++ Tournaments.Test/TournamentTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tournaments.Test
+{
+    [TestClass]
+    public class TournamentTest
+    {
+        [TestMethod]
+        public void WriteParticipants()
+        {
+
+        }
+    }
+}
